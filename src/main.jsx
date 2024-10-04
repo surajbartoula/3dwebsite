3a06da9e5f3c49a06fe6fb209864fcfc096914b2
surajbartoula <suraj_bartoula@yahoo.com>
2024-10-04 21:48:25 +0400

    swr next module issue persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Ground from './Ground.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+	<Ground/>
   </React.StrictMode>,
 )
